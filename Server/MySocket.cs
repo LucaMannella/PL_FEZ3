5e6b1a4dfa2be49cc60d3b03498e1c9292d4bdc4
stefano1007 <s214119@studenti.polito.it>
2016-05-24 11:18:10 +0200

    Codice server

---
+++ Server/MySocket.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace Server
+{
+    class MySocket
+    {
+        private Socket s;
+
+        public MySocket(Socket s)
+        {
+            this.s = s;
+        }
+
+        public bool Connected { get { return s.Connected; } }
+
+        public EndPoint RemoteEndPoint { get { return s.RemoteEndPoint; } }
+
+        public int ReceiveTimeout { get { return s.ReceiveTimeout; } set { s.ReceiveTimeout = value; } }
+
+        public int Receive(byte[] buffer, int lung, SocketFlags socketFlags)
+        {
+            int mancanti = lung;
+            int totRicevuti = 0;
+            int ricevuti;
+            while ((mancanti = lung - totRicevuti) > 0)
+            {
+                if ((ricevuti = s.Receive(buffer, totRicevuti, mancanti, SocketFlags.None)) == 0)
+                    return 0;
+                totRicevuti += ricevuti;
+            }
+            return lung;
+        }
+
+        internal void Shutdown(SocketShutdown w)
+        {
+            s.Shutdown(w);
+        }
+
+        internal void Close()
+        {
+            s.Close();
+        }
+
+        internal int Send(byte[] buffer, int lung, SocketFlags socketFlags)
+        {
+            return s.Send(buffer, lung, socketFlags);
+        }
+
+        internal int Send(byte[] bytes)
+        {
+            return s.Send(bytes);
+        }
+
+        internal void SendFile(string fileName)
+        {
+            s.SendFile(fileName);
+        }
+        public void receiveFile(string path_file, long lung)
+        {
+            FileStream fileW = File.Open(path_file, FileMode.Create);
+            byte[] buffer = new byte[20480];
+
+            long totRicevuti = 0;
+            int ricevuti;
+            long mancanti;
+            while ((mancanti = lung - totRicevuti) > 0)
+            {
+                if (mancanti >= 20480)
+                    ricevuti = s.Receive(buffer, 20480, SocketFlags.None);
+                else
+                    ricevuti = s.Receive(buffer, (int)mancanti, SocketFlags.None);
+                fileW.Write(buffer, 0, ricevuti);
+                totRicevuti += ricevuti;
+            }
+
+            fileW.Seek(0, SeekOrigin.Begin);            
+            fileW.Close();
+            return;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Server
 {
@@ -10,6 +12,41 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.StartListening();
+
+        }
+
+        private void StartListening()
+        {
+            Socket handler;            
+            //IPAddress ip = IPAddress.Parse("10.13.15.211");
+            //IPEndPoint localEndPoint = new IPEndPoint(ip, 1500);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 1500);            
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);            
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+               
+                // Start listening for connections.
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.
+                    handler = listener.Accept();                     
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected Error");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+                return;
+            }
         }
     }
 }
