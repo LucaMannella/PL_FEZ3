3ab39950236271bc246c7068c12ea647a25fa076
stefano1007 <s214119@studenti.polito.it>
2016-06-06 10:00:04 +0200

    classi interfaces client

---
+++ Client/PinWindow.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Runtime.CompilerServices;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation;
+
+using GT = Gadgeteer;
+using Microsoft.SPOT.Input;
+
+namespace Client
+{
+    class PinWindow : BaseWindow
+    {
+        public PinWindow()
+        {
+            AddTitleBar("Inserire PIN", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.Red, GT.Color.Blue, GT.Color.Blue);
+
+        }
+    }
+}
