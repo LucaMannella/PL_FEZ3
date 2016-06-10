f96f9c9bb9ef87a28daee3b23b84bf9a2f58fc3e
Luke <luca.mannella@studenti.polito.it>
2016-06-10 13:11:59 +0200

    Aggiunto file per generare automaticamente il database.

---
+++ pl_fez03.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Giu 10, 2016 alle 13:09
+-- Versione del server: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pl_fez03`
+--
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `clients`
+--
+CREATE TABLE IF NOT EXISTS `clients` (
+  `MAC` varchar(17) NOT NULL,
+  `Port` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `suspicious_pictures`
+--
+CREATE TABLE IF NOT EXISTS `suspicious_pictures` (
+  `MAC` char(17) NOT NULL,
+  `Timestamp` bigint(20) NOT NULL,
+  `Path` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `test`
+--
+CREATE TABLE IF NOT EXISTS `test` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `test`
+--
+INSERT INTO `test` (`ID`, `Name`) VALUES
+(1, 'Alfonso'),
+(2, 'Luca');
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`MAC`);
+
+--
+-- Indexes for table `suspicious_pictures`
+--
+ALTER TABLE `suspicious_pictures`
+  ADD PRIMARY KEY (`MAC`,`Timestamp`), ADD UNIQUE KEY `Path` (`Path`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
