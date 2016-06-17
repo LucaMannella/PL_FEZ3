8df199ac70a0fb6e0141d3b5ba75952449a5a4c5
Alfonso <s220389@studenti.polito.it>
2016-06-17 21:23:21 +0200

    sassdssdsd

--- Server/Constants.cs
+++ Server/Constants.cs
@@ -3,14 +3,14 @@
 public static class Constants
 {
     // modify also this address in Service.App.config
-    public static String SERVER_IP_ADDR = "192.168.56.1";
+    public static String SERVER_IP_ADDR = "192.168.137.1";
     public static int PORT = 1500;
-    
-    public static String IMAGE_DIRECTORY = @"C:\Users\Alfonso-LAPTOP\Desktop\image";
+
+    public static String IMAGE_DIRECTORY = @"C:\Users\Alfonso-LAPTOP\Desktop\FezImage\image";
 
     public static String MAIL_RECEIVER = "valenzise@tiscali.it";
     public static String MAIL_SENDER = "fez03noreply@gmail.com";
     public static String MAIL_SENDER_PASSWORD = "fez03password";
 
-    public static String SERVICE_URL = "http://192.168.56.1:8733/Service/";
+    public static String SERVICE_URL = "http://192.168.137.1:8733/Service/";
 }
